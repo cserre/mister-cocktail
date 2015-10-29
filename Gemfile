8add5830d1c15e37e21e7f5b4d27a246a547534e
Charlotte Serre <c.serre41@gmail.com>
2015-10-29 11:14:26 +0100

    Added figaro

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'font-awesome-sass', '~> 4.2.0'
 gem 'simple_form'
 gem "autoprefixer-rails"
 
+# security
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,6 +3,6 @@ class Dose < ActiveRecord::Base
   belongs_to :cocktail
 
   validates :description, presence: true
-  validates :cocktail_id, presence: true, uniqueness: { scope: :ingredient_id }
+  validates :cocktail_id, presence: true, uniqueness: { scope: :ingredient_id, message: "rrrr" }
   validates :ingredient_id, presence: true
 end
