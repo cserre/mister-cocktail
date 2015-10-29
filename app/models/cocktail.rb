a7c20315f4a560f44761844672dee4ee5834c862
Charlotte Serre <c.serre41@gmail.com>
2015-10-29 14:43:55 +0100

    cocktail picture mandatory

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,10 +1,9 @@
 class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_attached_file :picture,
-      styles: { medium: "300x300>" }
+  has_attached_file :picture, styles: { medium: "300x300>" }
 
-    validates_attachment_content_type :picture,
-      content_type: /\Aimage\/.*\z/
+  validates_attachment :picture, presence: true
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   validates :name, presence: true, uniqueness: true
 end
